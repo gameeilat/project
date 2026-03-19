67f9ae6a906dd1b88f133544d840e51d1632eb3b
gameeilat <115869991+gameeilat@users.noreply.github.com>
2026-03-19 05:03:33 +0000

    Mar 19, 2026, 5:03 AM

--- project/sidebar-loader.js
+++ project/sidebar-loader.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => { //waits for the page to lo
   // find project root automatically
   const depth = location.pathname.split("/").length - 2; //tales the amounts of returns and subtracts 2
   const prefix = depth > 0 ? "../".repeat(depth - 1) : ""; //returns it back to the root file
-  
+
   fetch(prefix + "sidebar.html")
     .then(res => {
       if (!res.ok) throw new Error("Sidebar not found");
